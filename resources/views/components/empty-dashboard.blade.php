5972f46b52b9fde3267c978d2326e03a47a2beb0
Kristelle Feghali <kristellefeghali@gmail.com>
2023-03-05 18:30:28 +0100

    fix vapor error: syntax error in empty dashboard

--- resources/views/components/empty-dashboard.blade.php
+++ resources/views/components/empty-dashboard.blade.php
@@ -3,11 +3,13 @@
         <a href="{{url('https://illustrious-perch-600.notion.site/Getting-started-10min-37dd19cc7503474a8409893aebacaaa6')}}" target="_blank" class="text-blue-700">
             these steps
         </a>
-        or book a call with our CTO.
+        or
+        <a href="{{url('https://calendly.com/kristellefeghali/30min')}}" class="text-blue-700">book a call</a>
+        with our CTO.
     </p>
 </div>
 
-<!-- Calendly inline widget begin -->
-<div class="calendly-inline-widget" data-url="https://calendly.com/kristellefeghali/30min" style="min-width:320px;height:630px;"></div>
-<script type="text/javascript" src="https://assets.calendly.com/assets/external/widget.js" async></script>
-<!-- Calendly inline widget end -->
+{{--<!-- Calendly inline widget begin -->--}}
+{{--<div class="calendly-inline-widget" data-url="https://calendly.com/kristellefeghali/30min" style="min-width:320px;height:630px;"></div>--}}
+{{--<script type="text/javascript" src="https://assets.calendly.com/assets/external/widget.js" async></script>--}}
+{{--<!-- Calendly inline widget end -->--}}
