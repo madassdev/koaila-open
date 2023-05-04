cd3217ab02ebb7ebf4193dd4466ae39f7174a509
Kristelle Feghali <kristellefeghali@gmail.com>
2023-05-04 17:42:20 +0200

    change-app-name-to-koaila

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Prophesee') }}</title>
+    <title>{{ config('app.name', 'Koaila') }}</title>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -24,7 +24,7 @@
     <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
         <div class="container">
             <a class="navbar-brand" href="{{ route('upsell-dashboard') }}">
-                {{ config('app.name', 'Prophesee') }}
+                {{ config('app.name', 'Koaila') }}
             </a>
 
             <div id="navbarSupportedContent">
