2d3dfa2b5cb8c7af0fd998553a28b388059081f6
Kristelle Feghali <kristellefeghali@gmail.com>
2023-05-04 19:21:36 +0200

    frontend-change-delete-button-in-upsell-dashboard

--- resources/views/components/dashboard/upsell-dashboard.blade.php
+++ resources/views/components/dashboard/upsell-dashboard.blade.php
@@ -104,7 +104,11 @@
                                 <form method="POST" action="{{ route('delete-customer-state', $customer->latestState->id) }}">
                                     {{ csrf_field() }}
                                     <div class="form-group">
-                                        <input type="submit" onclick="return confirm('Are you sure you want to delete this user from the upsell list?')" class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900" value="Delete">
+                                        <button type="submit" onclick="return confirm('Are you sure you want to delete this user from the upsell list?')" class="focus:outline-none text-white bg-red-500 hover:bg-red-700 focus:ring-4 focus:ring-red-300 font-medium text-sm p-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">
+                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
+                                                <path fill-rule="evenodd" d="M5.293 4.293a1 1 0 0 1 1.414 0L10 8.586l3.293-3.293a1 1 0 1 1 1.414 1.414L11.414 10l3.293 3.293a1 1 0 1 1-1.414 1.414L10 11.414l-3.293 3.293a1 1 0 0 1-1.414-1.414L8.586 10 5.293 6.707a1 1 0 0 1 0-1.414z" clip-rule="evenodd" />
+                                            </svg>
+                                        </button>
                                     </div>
                                 </form>
                             </td>
