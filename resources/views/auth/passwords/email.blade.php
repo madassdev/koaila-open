c1c45bbbc07df7bf18a74b4f7b8ba0914f3d72a0
Kristelle Feghali <kristellefeghali@gmail.com>
2023-05-16 11:29:29 +0200

    frontend-change-button-password-reset

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -33,7 +33,7 @@
 
                         <div class="row mb-0">
                             <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
                                     {{ __('Send Password Reset Link') }}
                                 </button>
                             </div>
